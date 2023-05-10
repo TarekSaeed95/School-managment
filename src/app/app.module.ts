b30130138ca88ad3b90a18e9eec5a8b1f5cc57ae
tarek95 <tare2sa3eed@gmail.com>
2023-05-10 23:00:14 +0300

    fix app module error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+import { NavbarComponent } from './Pages/HomePage/navbar/navbar.component';
+import { LandingsectionComponent } from './Pages/HomePage/landing-section/landing-section.component';
+import { BrightFutureSectionComponent } from './Pages/HomePage/bright-future-section/bright-future-section.component';
+import { TestimonialsSectionComponent } from './Pages/HomePage/testimonials-section/testimonials-section.component';
+import { HomePageComponent } from './Pages/HomePage/home-page/home-page.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LandingsectionComponent,
+    BrightFutureSectionComponent,
+    TestimonialsSectionComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule
