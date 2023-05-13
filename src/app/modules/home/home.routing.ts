a65e3b4ecd60a1dfd887b034570fb98adf4fdc45
tarek95 <tare2sa3eed@gmail.com>
2023-05-13 15:53:16 +0300

    before merging with origin

--- src/app/modules/home/home.routing.ts
+++ src/app/modules/home/home.routing.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomePageComponent } from './page/home-page.component';
+import { LoginComponent } from './login.component';
 
 export const routes: Routes = [
   {
@@ -11,6 +12,10 @@ export const routes: Routes = [
   {
     path: 'home',
     component: HomePageComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
   },
 ];
 
